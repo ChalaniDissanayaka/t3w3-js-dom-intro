b81c030c0eb36886842a4ffeff2f1a37054858d7
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-08-28 22:02:52 +1000

    added styles to dataList and button

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -1,23 +1,22 @@
 // Declare data to use in the logic
 let dataArray = [
-    "warthog",
-    "interceptor",
-    "herbie",
-    "ligthning mcqueen",
-    "batmobile",
-    "bat tumbler",
-    "cybertruck",
-    "delorean",
-    "elanore",
-    "magic school bus",
-    "toyota corolla"
+    "Warthog",
+    "Interceptor",
+    "Herbie",
+    "Lightning mcqueen",
+    "Batmobile",
+    "Bat tumbler",
+    "Cybertruck",
+    "Delorean",
+    "Elanore",
+    "Magic school bus",
+    "Toyota corolla"
 ];
 
 // Find elements on the page for us to use in the logic
 let carsContainerElement = document.getElementById("carsContainer");
 
-
-function renderData () {
+function renderData() {
 
     carsContainerElement.innerText = "";
 
@@ -32,19 +31,41 @@ function renderData () {
 
         // Create a new HTML element to help us format the data's value
         let newCarEntry = document.createElement("li");
-        // Add the data to the new element
-        newCarEntry.innerText += car;
+
+        // Set larger font size for the list items
+        newCarEntry.style.fontSize = "25px";
+
+        // Add the data to the new element with a space after the car name
+        newCarEntry.innerText = `${car} `;
 
         // Add a button to each entry
         // Click on the button to remove the entry from the list
         let removeButton = document.createElement("button");
-        // removeButton.innerText = "Remove" + car;
         removeButton.innerText = `Remove ${car}`;
+
+        // Set button styles
+        removeButton.style.backgroundColor = "lightyellow"; // Light yellow or cream color
+        removeButton.style.fontSize = "16px";
+        removeButton.style.padding = "6px 30px";
+        removeButton.style.cursor = "pointer";
+        removeButton.style.borderRadius = "5px";
+        removeButton.style.marginLeft = "15px"; // Space between car name and button
+
+        // Add hover effect for button color change
+        removeButton.onmouseover = () => {
+            removeButton.style.backgroundColor = "lightgreen";
+        };
+        removeButton.onmouseout = () => {
+            removeButton.style.backgroundColor = "lightyellow";
+        };
 
         removeButton.onclick = (() => removeCarFromDataList(car));
 
         // Add the removeButton to the car entry
         newCarEntry.appendChild(removeButton);
+
+        // Add space between list items
+        newCarEntry.style.marginBottom = "10px";
 
         // Add the nicely-formatted element into the list container
         carsContainerList.appendChild(newCarEntry);
@@ -54,13 +75,14 @@ function renderData () {
     carsContainerElement.appendChild(carsContainerList);
 }
 
-
-function removeCarFromDataList (tragetItemToRemove) {
+function removeCarFromDataList(targetItemToRemove) {
 
     // Remove the target item from the dataArray
-    dataArray = dataArray.filter((car) => car != tragetItemToRemove);
+    dataArray = dataArray.filter((car) => car !== targetItemToRemove);
 
     // After the filtering is done, re-render the page with the "new" array
     renderData();
+}
 
-};
+// Initial render
+renderData();

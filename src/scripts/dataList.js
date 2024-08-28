f704d8c45e8e2e42cc2e8410413567c6f9d2809b
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-08-28 20:40:26 +1000

    remove button now exists, no functionality

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -1,3 +1,4 @@
+// Declare data to use in the logic
 let dataArray = [
     "warthog",
     "interceptor",
@@ -12,17 +13,34 @@ let dataArray = [
     "toyota corolla"
 ];
 
+// Find elements on the page for us to use in the logic
 let carsContainerElement = document.getElementById("carsContainer");
+
+// Create elements to add to the page via the logic
 let carsContainerList = document.createElement("ul");
 
+// Do logic for each individual item in the array
 dataArray.forEach((car) => {
+
+    // Verify what we are working with, just log it to see the data's value
     console.log(car);
 
+    // Create a new HTML element to help us format the data's value
     let newCarEntry = document.createElement("li");
-
+    // Add the data to the new element
     newCarEntry.innerText += car;
 
+    // Add a button to each entry
+    // Click on the button to remove the entry from the list
+    let removeButton = document.createElement("button");
+    // removeButton.innerText = "Remove" + car;
+    removeButton.innerText = `Remove ${car}`;
+    // Add the removeButton to the car entry
+    newCarEntry.appendChild(removeButton);
+
+    // Add the nicely-formatted element into the list container
     carsContainerList.appendChild(newCarEntry);
 });
 
+// Add the list container onto the actual HTML page
 carsContainerElement.appendChild(carsContainerList);

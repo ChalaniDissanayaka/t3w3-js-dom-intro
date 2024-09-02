260053d93003481790bfba00bab8e96fa3b8b8b2
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-09-02 20:06:58 +1000

    saving and loading theme working

--- src/scripts/cssThemeToggler.js
+++ src/scripts/cssThemeToggler.js
@@ -22,14 +22,47 @@ let themes = [
 // Read theme name stored in local storage
 // and update CSS variables based on that name
 function getChosenTheme() {
-
+    let foundTheme = localStorage.getItem("theme");
+    console.log(foundTheme);
+    return foundTheme;
 }
 
 // Set theme name stored in local storage
 // and update CSS variables based on that name
-function setChosenTheme() {
-    
+function setChosenTheme(newThemeName) {
+    localStorage.setItem("theme", newThemeName);
 }
+
+if (getChosenTheme() == null) {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    if(darkThemeMq.matches) {
+        // theme set to dark.
+        setChosenTheme("dark");
+        console.log("No theme found, applied the dark theme")
+    } else {
+        // theme set to light.
+        setChosenTheme("light");
+        console.log("No theme found, applied the light theme")
+    }
+}
+
+
+function toggleTheme() {
+    // if ("dark" == "dark"){
+    // if ("light" == "dark"){
+    if(getChosenTheme() == "dark"){
+        // set it to light
+        setChosenTheme("light");
+    } else {
+        // set it to dark
+        setChosenTheme("dark");
+    };
+}
+
+
+let themeToggleButton = document.getElementById("themeToggle");
+themeToggleButton.onclick = toggleTheme;
+// themeToggleButton.addEventListener("click", toggleTheme);
 
 // Loop through properties key in chosen theme object
 // and apply those properties to CSS
